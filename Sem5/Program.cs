0e2b889d4490c05bf87afe58a78838dc1ede7e5c
Tatyana <turina_tatyana@mail.ru>
2022-08-20 22:55:37 +0300

    Sem5

---
+++ Sem5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Новая_папка
+
+
+{
+
+}
+class Program
+{
+
+   static void WriteArray(int[] array)
+   {
+       for(int i = 0; i < array.Length; i++)
+       {
+          Console.Write(array[i] + " ");
+       }
+   }
+
+static void Main(string[] args)
+{
+    string number =Console.ReadLine();  //1,24,613,124
+    string[] array = new string[number.Length];
+    int index = 0;
+    foreach (var item in number)  //1, 2 4 , 6 1 3 , 1 2 4
+    {
+        if(item != ',')
+        {
+            array[index] += item.ToString();
+        } else {
+    
+            index++;
+        }
+    }
+    Console.Write("[");
+    for(int i = 0; i <= index-1; i++)
+    {
+        Console.Write(array[i] +  ", ");
+    }
+    Console.Write(array[index] + "]");
+}
+}
